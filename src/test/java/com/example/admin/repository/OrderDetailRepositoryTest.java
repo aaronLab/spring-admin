6c758a48c6f14e9022490a059da818c2a980791c
Aaron Lee <aaronlab.net@gmail.com>
2021-01-24 19:34:08 +0900

    order detail repository test

--- src/test/java/com/example/admin/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/example/admin/repository/OrderDetailRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -16,17 +17,21 @@ public class OrderDetailRepositoryTest extends AdminApplicationTests {
     private OrderDetailRepository orderDetailRepository;
 
     @Test
-    @Transactional
     public void create() {
         OrderDetail orderDetail = new OrderDetail();
 
-//        orderDetail.setOrderAt(LocalDateTime.now());
+        orderDetail.setStatus("WAITING");
+        orderDetail.setArrivalDate(LocalDateTime.now().plusDays(2));
+        orderDetail.setQuantity(1);
+        orderDetail.setTotalPrice(BigDecimal.valueOf(3000));
 
-//        orderDetail.setItemId(1L);
-//        orderDetail.setUserId(1L);
+        orderDetail.setOrderGroupId(1L); // CART
+        orderDetail.setItemId(1L);
+
+        orderDetail.setCreatedAt(LocalDateTime.now());
+        orderDetail.setCreatedBy("AdminServer");
 
         OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
-
         Assertions.assertNotNull(newOrderDetail);
     }
 
