afa3c0281da4f4c08ea5125b4255f8c6248728e1
Aaron Lee <aaronlab.net@gmail.com>
2021-01-24 19:48:00 +0900

    order group + admin user repository test

---
+++ src/test/java/com/example/admin/repository/OrderGroupRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.example.admin.repository;
+
+import com.example.admin.AdminApplicationTests;
+import com.example.admin.model.entity.OrderGroup;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OrderGroupRepositoryTest extends AdminApplicationTests {
+
+    @Autowired
+    private OrderGroupRepository orderGroupRepository;
+
+    @Test
+    public void create() {
+
+        OrderGroup orderGroup = new OrderGroup();
+
+        orderGroup.setStatus("COMPLETE");
+        orderGroup.setOrderType("ALL");
+        orderGroup.setRevAddress("Seoul, KR");
+        orderGroup.setRevName("Aaron");
+        orderGroup.setPaymentType("CARD");
+        orderGroup.setTotalPrice(BigDecimal.valueOf(3000));
+        orderGroup.setTotalQuantity(1);
+        orderGroup.setOrderAt(LocalDateTime.now().minusDays(2));
+        orderGroup.setArrivalDate(LocalDateTime.now());
+        orderGroup.setCreatedAt(LocalDateTime.now());
+        orderGroup.setCreatedBy("AdminServer");
+        orderGroup.setUserId(1L);
+
+        OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+        Assertions.assertNotNull(newOrderGroup);
+    }
+
+}
