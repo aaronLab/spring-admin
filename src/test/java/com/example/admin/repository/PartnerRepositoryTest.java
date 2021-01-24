3d6191c6eb1302b022f845ee76a1ddc26ec4e581
Aaron Lee <aaronlab.net@gmail.com>
2021-01-24 19:09:57 +0900

    partner test

---
+++ src/test/java/com/example/admin/repository/PartnerRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.example.admin.repository;
+
+import com.example.admin.AdminApplicationTests;
+import com.example.admin.model.entity.Partner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class PartnerRepositoryTest extends AdminApplicationTests {
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Test
+    public void create() {
+        String name = "Partner01";
+        String status = "REGISTERED";
+        String address = "Seoul, KR";
+        String callCenter = "02-0000-0000";
+        String partnerNumber = "02-0000-0000";
+        String businessNumber = "1234567890123";
+        String ceoName = "Aaron";
+        LocalDateTime registeredAt = LocalDateTime.now();
+        LocalDateTime unregisteredAt = LocalDateTime.now();
+        LocalDateTime createdAt = LocalDateTime.now();
+        String createdBy = "AdminServer";
+        Long categoryId = 1L;
+
+        Partner partner = new Partner();
+        partner.setName(name);
+        partner.setStatus(status);
+        partner.setAddress(address);
+        partner.setCallCenter(callCenter);
+        partner.setPartnerNumber(partnerNumber);
+        partner.setBusinessNumber(businessNumber);
+        partner.setCeoName(ceoName);
+        partner.setRegisteredAt(registeredAt);
+        partner.setUnregisteredAt(unregisteredAt);
+        partner.setCreatedAt(createdAt);
+        partner.setCreatedBy(createdBy);
+        partner.setCategoryId(categoryId);
+
+        Partner newPartner = partnerRepository.save(partner);
+
+        Assertions.assertNotNull(newPartner);
+        Assertions.assertNotNull(newPartner.getName(), name);
+    }
+
+}
