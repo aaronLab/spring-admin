86483247f0841ef91f07417d5c2bbc47ac2f8f12
Aaron Lee <aaronlab.net@gmail.com>
2021-01-25 22:27:32 +0900

    user create api

--- src/main/java/com/example/admin/service/UserApiLogicService.java
+++ src/main/java/com/example/admin/service/UserApiLogicService.java
@@ -1,6 +1,7 @@
 package com.example.admin.service;
 
 import com.example.admin.ifs.CrudInterface;
+import com.example.admin.model.entity.User;
 import com.example.admin.model.network.Header;
 import com.example.admin.model.network.request.UserApiRequest;
 import com.example.admin.model.network.response.UserApiResponse;
@@ -8,16 +9,31 @@ import com.example.admin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
 
     @Autowired
     private UserRepository userRepository;
 
-
     @Override
-    public Header<UserApiResponse> create(UserApiRequest request) {
-        return null;
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        UserApiRequest userApiRequest = request.getData();
+
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        User newUser = userRepository.save(user);
+
+        return response(newUser);
     }
 
     @Override
@@ -26,13 +42,28 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     }
 
     @Override
-    public Header<UserApiResponse> update(UserApiRequest request) {
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
         return null;
     }
 
     @Override
     public Header delete(Long id) {
         return null;
+    }
+
+    private Header<UserApiResponse> response(User user) {
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword()) // todo mask
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unRegisteredAt(user.getUnregisteredAt())
+                .build();
+
+        return Header.OK(userApiResponse);
     }
 
 }
