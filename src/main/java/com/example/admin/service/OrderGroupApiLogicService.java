ad334bbec1fb329d4de584df8c4ba2031e3b900d
Aaron Lee <aaronlab.net@gmail.com>
2021-01-27 20:47:52 +0900

    order group service

--- src/main/java/com/example/admin/service/OrderGroupApiLogicService.java
+++ src/main/java/com/example/admin/service/OrderGroupApiLogicService.java
@@ -1,36 +1,120 @@
 package com.example.admin.service;
 
 import com.example.admin.ifs.CrudInterface;
+import com.example.admin.model.entity.OrderGroup;
 import com.example.admin.model.network.Header;
 import com.example.admin.model.network.request.OrderGroupApiRequest;
 import com.example.admin.model.network.response.OrderGroupApiResponse;
 import com.example.admin.repository.OrderGroupRepository;
+import com.example.admin.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
 
     private final OrderGroupRepository orderGroupRepository;
+    private final UserRepository userRepository;
 
     @Override
     public Header<OrderGroupApiResponse> create(Header<OrderGroupApiRequest> request) {
-        return null;
+
+        OrderGroupApiRequest body = request.getData();
+
+        OrderGroup orderGroup = OrderGroup
+                .builder()
+                .status(body.getStatus())
+                .orderType(body.getOrderType())
+                .revAddress(body.getRevAddress())
+                .revName(body.getRevName())
+                .paymentType(body.getPaymentType())
+                .totalPrice(body.getTotalPrice())
+                .totalQuantity(body.getTotalQuantity())
+                .arrivalDate(body.getArrivalDate())
+                .orderAt(LocalDateTime.now())
+                .createdBy(body.getCreatedBy())
+                .updatedAt(LocalDateTime.now())
+                .updatedBy(body.getUpdatedBy())
+                .user(userRepository.getOne(body.getUserId()))
+                .build();
+
+        OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+
+        return response(newOrderGroup);
     }
 
     @Override
     public Header<OrderGroupApiResponse> read(Long id) {
-        return null;
+
+        Optional<OrderGroup> optionalOrderGroup = orderGroupRepository.findById(id);
+
+        return optionalOrderGroup
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("no order group data"));
     }
 
     @Override
     public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
-        return null;
+
+        OrderGroupApiRequest body = request.getData();
+
+        return orderGroupRepository.findById(body.getId())
+                .map(orderGroup -> {
+                    orderGroup
+                            .setStatus(body.getStatus())
+                            .setOrderType(body.getOrderType())
+                            .setRevAddress(body.getRevAddress())
+                            .setRevName(body.getRevName())
+                            .setPaymentType(body.getPaymentType())
+                            .setTotalPrice(body.getTotalPrice())
+                            .setTotalQuantity(body.getTotalQuantity())
+                            .setArrivalDate(body.getArrivalDate())
+                            .setUpdatedAt(LocalDateTime.now())
+                            .setUpdatedBy(body.getUpdatedBy());
+
+                    return orderGroup;
+                })
+                .map(orderGroupRepository::save)
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("no order group data"));
     }
 
     @Override
     public Header delete(Long id) {
-        return null;
+        return orderGroupRepository.findById(id)
+                .map(orderGroup -> {
+                    orderGroupRepository.delete(orderGroup);
+
+                    return Header.OK("success");
+                })
+                .orElseGet(() -> Header.ERROR("no order group data"));
     }
+
+    private Header<OrderGroupApiResponse> response(OrderGroup orderGroup) {
+        OrderGroupApiResponse body = OrderGroupApiResponse
+                .builder()
+                .id(orderGroup.getId())
+                .status(orderGroup.getStatus())
+                .orderType(orderGroup.getOrderType())
+                .revAddress(orderGroup.getRevAddress())
+                .revName(orderGroup.getRevName())
+                .paymentType(orderGroup.getPaymentType())
+                .totalPrice(orderGroup.getTotalPrice())
+                .totalQuantity(orderGroup.getTotalQuantity())
+                .orderAt(orderGroup.getOrderAt())
+                .arrivalDate(orderGroup.getArrivalDate())
+                .createdAt(orderGroup.getCreatedAt())
+                .createdBy(orderGroup.getCreatedBy())
+                .updatedAt(orderGroup.getUpdatedAt())
+                .updatedBy(orderGroup.getUpdatedBy())
+                .userId(orderGroup.getUser().getId())
+                .build();
+
+        return Header.OK(body);
+    }
+
 }
